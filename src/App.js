f66ad7f966a29c84eb9767c4aef07a492c25d72e
sangho <andrew12ksh@gmail.com>
2018-06-13 12:16:44 +0900

    add phoneform and phoneInfo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import PhoneForm from './components/PhoneForm'
 import './App.css';
 
 class App extends Component {
+  id=2
+  state={
+    information:[
+      {
+        id:0,
+        name : '김민준',
+        phone :'010-0000-0000'
+      },
+      {
+        id:1,
+        name : '황윤선',
+        phone :'010-0000-0000'
+      }
+    ]
+  }
+  handleCreate = (data) =>{
+    const {information} = this.state;
+    this.setState({
+      information: information.concat({id : this.id++, ...data})
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <PhoneForm
+        onCreate={this.handleCreate}
+      />
+      {JSON.stringify(this.state.information)}
       </div>
     );
   }
